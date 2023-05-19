45f15807fa3d0d727c689ea578938865e788bcf9
NivethaAshwi <133736462+NivethaAshwi@users.noreply.github.com>
2023-05-19 13:28:46 +0530

    Update and rename ResidentdetailService.cs to ResidentDetailService.cs

--- VisitorManagement.Service/service/ResidentDetailService.cs
+++ VisitorManagement.Service/service/ResidentDetailService.cs
@@ -12,40 +12,41 @@ using VisitorManagement.Service.IRepoInfo;
 
 namespace VisitorManagement.Service.service
 {
-    public class ResidentdetailService : IResidentRenpo
+    public class ResidentDetailService : IResidentRepository
     {
-        private readonly VisitorCategoryDBContext _dbforRDdetails;  //RD - Resident details
-        public ResidentdetailService(VisitorCategoryDBContext dBContext)
+        private readonly VisitorCategoryDBContext _residentDbContext;  
+        public ResidentDetailService(VisitorCategoryDBContext dBContext)
         {
-            _dbforRDdetails = dBContext;
+            _residentDbContext = dBContext;
         }
         public async Task CreateResident(ResidentDetails visitors)
         {
-            await _dbforRDdetails.ResidentDetails.AddAsync(visitors);
-            await save();
+            await _residentDbContext.ResidentDetails.AddAsync(visitors);
+            await Save();
         }
         public async Task DeleteResident(ResidentDetails visitordetails)
         {
-            _dbforRDdetails.ResidentDetails.Remove(visitordetails);
-            await save();
+            _residentDbContext.ResidentDetails.Remove(visitordetails);
+            await Save();
         }
         public async Task<List<ResidentDetails>> GetAllResident()
         {
-            var residents = await _dbforRDdetails.ResidentDetails.Include(a => a.LogDetails).ToListAsync();
+            var residents = await _residentDbContext.ResidentDetails.ToListAsync();
+            // var residents = await _dbforRDdetails.ResidentDetails.Include(a => a.LogDetails).ToListAsync()
             return residents;
         }
         public async Task<List<ResidentDetails>> GetResidentById(int id)
         {
-            var residentbyId = await _dbforRDdetails.ResidentDetails.Where(a => a.ResidentId == id).Include(_ => _.LogDetails).ToListAsync();
+            var residentbyId = await _residentDbContext.ResidentDetails.Where(a => a.ResidentId == id).Include(_ => _.LogDetails).ToListAsync();
             return residentbyId;
         }
-        public async Task save()
+        public async Task Save()
         {
-            await _dbforRDdetails.SaveChangesAsync();
+            await _residentDbContext.SaveChangesAsync();
         }
         public async Task<IEnumerable<ResidentDetails>> SearchByDetails(string FlatID, string FN, string LN)
         {
-            IQueryable<ResidentDetails> query = _dbforRDdetails.ResidentDetails;
+            IQueryable<ResidentDetails> query = _residentDbContext.ResidentDetails;
             if(!string.IsNullOrEmpty(FlatID))
             {
                 query = query.Where(d => d.Flatnumber.Contains(FlatID));
@@ -65,14 +66,14 @@ namespace VisitorManagement.Service.service
         public async Task<ResidentDetails> FindResidentsInfo(int id)
         {
 
-            var findRD = await _dbforRDdetails.ResidentDetails.FindAsync(id);
+            var findRD = await _residentDbContext.ResidentDetails.FindAsync(id);
             return findRD;
 
         }
         public async Task UpdateResident(ResidentDetails visitordetails)
         {
-            _dbforRDdetails.ResidentDetails.Update(visitordetails);
-            await save();
+            _residentDbContext.ResidentDetails.Update(visitordetails);
+            await Save();
         }
     }
 }
