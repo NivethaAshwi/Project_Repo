1bfbe0ef27de91a6e3d97a46df3d381575f021a0
NivethaAshwi <133736462+NivethaAshwi@users.noreply.github.com>
2023-05-19 12:09:12 +0530

    Add files via upload

---
+++ VisitorManagement.Models/ViewModels/PaginationResultVM.cs
@@ -0,0 +1,23 @@
+﻿namespace VisitorManagement.API.ViewModels
+{
+    public class PaginationResultVM<T>
+    {
+        public int CurrentPage { get; set; }
+
+        public int TotalNoOfRecords { get; set; } 
+        public int PageSize { get; set; } //
+        public int TotalPages { get; set; } 
+        public List<T> Items { get; set; } //create as generic ..To utilize to all controllet generic used 
+        public bool HasPrevious => CurrentPage > 1; //  2 > 1 : 1
+        public bool HasNext => CurrentPage < TotalPages ;//99 < 100 :true 
+        public PaginationResultVM(int currentPage, int totalNoOfRecords, int pageSize, int totalPages, List<T> items)
+        {
+            CurrentPage = currentPage;
+            TotalNoOfRecords = totalNoOfRecords;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            Items = items;
+        }
+
+    }
+}
